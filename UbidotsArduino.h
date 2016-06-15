111dc7fa6da510931d84362c24a3d6e878a0ced6
Metavix <Metavix@localhost.localdomain>
2016-06-15 09:43:47 -0500

    fixed a bug of hung up

--- UbidotsArduino.h
+++ UbidotsArduino.h
@@ -1,7 +1,26 @@
 /*
-Ubidots.cpp -El objetivo de la librería es realizar la conexión con la plataforma ubidots, para que los usuarios puedan postear o escribir su información 
-en la nube
+Copyright (c) 2013-2016 Ubidots.
 
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Made by Mateo Velez - Metavix for Ubidots Inc
 
 */
 #ifndef _UbidotsArduino_H_
@@ -12,6 +31,7 @@ en la nube
 
 #define SERVER "things.ubidots.com"
 #define PORT 80
+#define MAX_VALUES 5
 
 typedef struct Value {
   char  *id;
@@ -19,19 +39,18 @@ typedef struct Value {
 } Value;
 
 class Ubidots {
-   public:
-      Ubidots(char* token);
-      bool sendAll();
-      float getValue(char* id);
-      void add(char *variable_id, double value);
-   
-   private:
-      char* _token;
-      uint8_t maxValues;
-      uint8_t currentValue;
-      Value * val;
-      WiFiClient _client;
-   
+ public:
+    Ubidots(char* token, char* server = SERVER);
+    bool sendAll();
+    bool getValue(char* id, float num = 0);
+    void add(char *variable_id, float value);
+
+ private:
+    char* _token;
+    char* _server;
+    uint8_t currentValue;
+    Value * val;
+    WiFiClient _client;
 };
 
-#endif
+#endif  // _UbidotsArduino_H_
